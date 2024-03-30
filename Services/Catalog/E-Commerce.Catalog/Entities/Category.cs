9173184b1745ce4ccc26c04f57f53b24a6e4d620
berkinmentas <berkinmentas@gmail.com>
2024-03-30 19:33:46 +0300

    Create entities

---
+++ Services/Catalog/E-Commerce.Catalog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace E_Commerce.Catalog.Entities
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ Services/Catalog/E-Commerce.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace E_Commerce.Catalog.Entities
+{
+    public class ProductImage
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string ProductImageID { get; set; }
+
+        public string Image1  { get; set; }
+        public string Image2  { get; set; }
+        public string Image3  { get; set; }
+        public string ProductId { get; set; }
+
+        [BsonIgnore]
+
+        public Product Product { get; set; }
+    }
+}
